89bdae8705a790eabc2696c9267c6c1274a3f980
cryosx <cyruswu.email@gmail.com>
2018-03-19 21:16:28 -1000

    Finished

--- secret_number.js
+++ secret_number.js
@@ -1,5 +1,9 @@
 'use strict';
+
 module.exports = function() {
-  let secretNum = Math.random() * 1000000;
-  return secretNum;
+    let secretNum = Math.random() * 1000000;
+    function secretNumber() {
+       return secretNum;
+    };
+    return secretNumber;
 };
